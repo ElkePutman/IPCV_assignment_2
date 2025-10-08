0e7806f2985c7572d737de7e18ef4b40bba1d01a
{ElkePutman} <{e.j.putman@student.utwente.nl}>
2025-10-08 23:29:11 +0200

    added some final clean-ups

---
+++ Elke_Putman_s2624257_assignment2/main.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+import os
+
+
+from Processor_class import VideoProcessor
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='OpenCV video processing')
+    parser.add_argument('-i', "--input", help='input video filename (zonder path)')
+    parser.add_argument('-o', "--output", help='output video filename (zonder path)')
+    args = parser.parse_args()
+
+    if args.input is None or args.output is None:
+        sys.exit("Please provide input and output video filenames! See --help")
+
+   
+    BASE_INPUT_PATH = os.path.dirname(os.path.abspath(__file__))
+    
+    BASE_OUTPUT_PATH = os.path.join(BASE_INPUT_PATH,"Processed_videos")
+    os.makedirs(BASE_OUTPUT_PATH, exist_ok=True)
+
+    input_file = os.path.join(BASE_INPUT_PATH, args.input)
+    output_file = os.path.join(BASE_OUTPUT_PATH, args.output)
+    
+
+    process = VideoProcessor(input_file, output_file)
+    process(show_video=False)
+
